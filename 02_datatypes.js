92e32db5d6f4f1c43dd208e2adbbe0e4965865b4
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2025-05-16 22:27:17 +0530

    Upload 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+// alert("Hello") //we are using nodejs, not browser
+
+console.log("Ayush")
+
+let name = "ayush" // string
+let age = 18 // number
+let isLoggedIn = false // boolean (true/false)
+
+// null ==> standalone value 
+//undefines => value is not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof isLoggedIn);
