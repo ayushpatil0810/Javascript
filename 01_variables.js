e2c367515035a9d08abe627097a9683cc745a2ca
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2025-05-16 21:45:46 +0530

    Upload 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accounrEmail = "example@company.com"
+var accountPassword = "12345"
+accountCity = "Nashik"
+let accountState
+
+// accountId = 2  // not allowed
+
+console.log(accountId)
+
+accounrEmail = "ap@gmail.com"
+accountPassword = "541"
+accountCity = "Mumbai"
+
+console.table([accountId, accounrEmail, accountPassword, accountCity, accountState])
