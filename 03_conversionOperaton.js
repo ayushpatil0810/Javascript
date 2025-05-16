9fd0acad7162f47e40d37b8da5157f3118048a37
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2025-05-16 23:00:02 +0530

    upload 03_conversionOperaton.js

---
+++ 03_conversionOperaton.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//number
+//"33" => 33
+// "33abc" => NaN // datatype of Nan is number
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//boolean
+// 1 => true; 0 => false
+// "" => false
+// "ayushpatil" => true
+
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber) 
